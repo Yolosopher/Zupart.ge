0c046e3aa83f6ab76267cc6a59e5962d09a200cc
Nika Nishnianidze <nika.nishnianidze.1@iliauni.edu.ge>
2020-08-17 17:42:09 +0400

    a

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let title = document.title;
+    let links = document.querySelectorAll('.header-nav-ul-li');
+    
+    links = [...links];
+
+    let filteredLink = links.filter(link => link.children[0].innerHTML === title)
+    filteredLink[0].classList.add('active');
+});
