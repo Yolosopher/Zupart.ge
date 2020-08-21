0aa1300f52744bd032405c526b93a93f13693ee7
Nika Nishnianidze <nika.nishnianidze.1@iliauni.edu.ge>
2020-08-21 15:16:01 +0400

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+function respoMenu() {
+    let width = window.innerWidth;
+    console.log(width);
+    if (width < 768) {
+        let activeText = $('.header-nav-ul-li.active').find('a').text();
+        console.log(activeText);
+        let menuSpan = $('.respo-fixed-ul-li');
+        menuSpan = [...menuSpan];
+        let filteredSpan = menuSpan.filter(span => span.children[0].children[1].innerHTML === activeText);
+        if (!filteredSpan[0]) {
+            return;
+        }
+        filteredSpan[0].children[0].classList.add('active');
+    }
+
+};
+
+
 document.addEventListener('DOMContentLoaded', ()=>{
     let title = document.title;
     let links = document.querySelectorAll('.header-nav-ul-li');
@@ -7,5 +25,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     let filteredLink = links.filter(link => link.children[0].innerHTML === title)
     if (filteredLink[0]) filteredLink[0].classList.add('active');
     
+
+
+    respoMenu();
 });
 
